8d2284612d3dbd8aaaff751fc912f7366714de31
nightskyshop1023 <nightskyshop1023@naver.com>
2023-12-24 21:38:50 +0900

    Design: Create Header Style

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,36 @@
 import Link from "next/link";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div>
-      <ul>
-        <li><Link href="/post">게시판</Link></li>
+    <header className={styles.header}>
+      <div className={styles.header__logo}>
+        <Link href="/">로고</Link>
+      </div>
 
-        <li><Link href="/post/create">게시판 글쓰기</Link></li>
+      <ul className={styles.header__list}>
+        <li className={styles.header__postlist}>
+          <Link href="/post">게시판</Link>
+        </li>
 
-        <li><Link href="/data-referance">데이터 자료실</Link></li>
+        <li className={styles.header_postcreate}>
+          <Link href="/post/create">게시판 글쓰기</Link>
+        </li>
 
+        <li className={styles.header_datareferance}>
+          <Link href="/data-referance">데이터 자료실</Link>
+        </li>
       </ul>
-    </div>
+
+      <div className={styles.header__user}>
+        <Link href="/login">로그인</Link>
+        <Link href="/signup">회원가입</Link>
+
+        {/* <Link href="/logout" className={styles.header__user}>로그아웃</Link>
+        <Link href="/프로필" className={styles.header__profileimg}>
+          프로필 이미지
+        </Link> */}
+      </div>
+    </header>
   )
 }
