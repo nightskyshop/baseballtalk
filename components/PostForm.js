5c3f7ce83c5ea63395c982cd8db60cfa78f486ac
nightskyshop1023 <nightskyshop1023@naver.com>
2024-01-05 23:58:15 +0900

    Design: Change Post Create Style

--- components/PostForm.js
+++ components/PostForm.js
@@ -1,7 +1,9 @@
-import axios from "axios";
+import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/router";
 import { useState } from "react";
 import styles from "./PostForm.module.css";
+import axios from "axios";
 
 export default function PostForm() {
   const router = useRouter();
@@ -41,12 +43,38 @@ export default function PostForm() {
         <button>등록</button>
       </div>
 
-      <input
-        type="text"
-        placeholder="제목을 입력해 주세요."
-        name="title"
-        className={styles.form__title}
-      />
+      <div className={styles.form__team}>
+        <div className={styles.form__selects}>
+          <div className={styles.form__select}>
+            <select name="team">
+              <option value="">팀를 선택해주세요.</option>
+              <option vlaue="LG Twins">LG Twins</option>
+              <option vlaue="KT Wiz">KT Wiz</option>
+              <option vlaue="SSG Landers">SSG Landers</option>
+              <option vlaue="NC Dinos">NC Dinos</option>
+              <option vlaue="Doosan Bears">Doosan Bears</option>
+              <option vlaue="KIA Tigers">KIA Tigers</option>
+              <option vlaue="Lotte Giants">Lotte Giants</option>
+              <option vlaue="Samsung Lions">Samsung Lions</option>
+              <option vlaue="Hanwha Eagles">Hanwha Eagles</option>
+              <option vlaue="Kiwoom Heros">Kiwoom Heros</option>
+            </select>
+            <FontAwesomeIcon icon={faCaretDown} />
+          </div>
+          
+          <div className={styles.form__select}>
+            <select name="category">
+              <option value="">주제를 선택해주세요.</option>
+            </select>
+            <FontAwesomeIcon icon={faCaretDown} />
+          </div>
+        </div>
+        <input
+          type="text"
+          placeholder="제목을 입력해 주세요."
+          name="title"
+        />
+      </div>
 
       <textarea
         type="text"
